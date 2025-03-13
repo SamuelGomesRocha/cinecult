75aa6af983ee82435e47520f80fe65507c3b31f8
SamuelGomesRocha <SamuelGomesRocha>
2025-03-12 21:26:01 -0300

    Inicialização do projeto.

---
+++ src/main/java/br/ufg/cinecult/CinecultApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.cinecult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinecultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinecultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufg/cinecult/CinecultApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.cinecult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinecultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
